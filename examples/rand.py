4e445068fea897f9f14a33a1c04dc8640a3cad88
Vlad Gheorghiu <vsoftco@gmail.com>
2020-02-06 11:07:27 -0500

    test for Windows when switching RNGs

--- examples/rand.py
+++ examples/rand.py
@@ -1,5 +1,6 @@
 # various RNGs Python example
 
+import platform  # to learn the OS we're on
 import oqs.rand as oqsrand  # must be explicitly imported
 
 #######################################################################
@@ -16,8 +17,10 @@ oqsrand.randombytes_nist_kat_init(bytes(entropy_seed))
 oqsrand.randombytes_switch_algorithm('NIST-KAT')
 print('{:17s}'.format("NIST-KAT:"), ' '.join('{:02X}'.format(x) for x in oqsrand.randombytes(32)))
 
-oqsrand.randombytes_switch_algorithm("OpenSSL")
-print('{:17s}'.format("OpenSSL:"), ' '.join('{:02X}'.format(x) for x in oqsrand.randombytes(32)))
+# we do not yet support OpenSSL under Windows
+if platform.system() != 'Windows':
+    oqsrand.randombytes_switch_algorithm("OpenSSL")
+    print('{:17s}'.format("OpenSSL:"), ' '.join('{:02X}'.format(x) for x in oqsrand.randombytes(32)))
 
 oqsrand.randombytes_switch_algorithm("system")
 print('{:17s}'.format("System (default):"), ' '.join('{:02X}'.format(x) for x in oqsrand.randombytes(32)))
