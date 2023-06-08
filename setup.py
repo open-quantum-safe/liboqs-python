310da67e0a6153b1deb4c233f3e2f4608ba327aa
Vlad Gheorghiu <vsoftco@gmail.com>
2023-06-08 16:13:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
+from setuptools import find_packages
 from distutils.core import setup
 
-setup()
+setup(
+    name='liboqs-python',
+    version='0.8.0',
+    author='Open Quantum Safe project',
+    author_email='contact@openquantumsafe.org',
+    packages=find_packages(exclude=('tests', 'docs', 'examples')),
+    scripts=[],
+    url='https://github.com/open-quantum-safe/liboqs-python',
+    license='LICENSE.txt',
+    description='Python wrapper for liboqs, providing post-quantum public key cryptography algorithms',
+    long_description=open('README.md').read(),
+)
